64da4631a7b77ffe896f45a1ed4e1f241f733db5
Sergey <104456968+Solod-S@users.noreply.github.com>
2025-06-08 12:05:09 +0300

    test
    test

--- public/wp-content/themes/dev_portfolio/functions.php
+++ public/wp-content/themes/dev_portfolio/functions.php
@@ -1,5 +1,6 @@
 <?php
-if (!defined('ABSPATH')) exit;
+if (!defined('ABSPATH'))
+	exit;
 
 // Define constants
 if (!defined('_VERSION')) {
@@ -11,3 +12,12 @@ require get_template_directory() . '/inc/_scripts.php';
 
 // Post types
 require get_template_directory() . '/inc/_post-types.php';
+
+// Register menus
+function register_my_menus()
+{
+	register_nav_menus(array(
+		'header-menu' => __('Header Menu'),
+		'footer-menu' => __('Footer Menu'),
+	));
+}
